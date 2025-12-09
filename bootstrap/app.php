7e70b317937ed2ae30ad1f04a05d87e7f0591228
ariffiandro <sinyaru562@gmail.com>
2025-12-09 17:26:39 +0700

    Add API login, user, logout controller + routes for Flutter

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => App\Http\Middleware\RoleMiddleware::class,
+            'auth' => \App\Http\Middleware\Authenticate::class,
         ]);
     })
     
